04e16d1fa103925312f6ea373614467b7e224073
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2019-08-02 12:08:41 +0900

    -

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
-import { serve } from "https://deno.land/std@v0.11/http/server.ts";
+import { serve } from "https://deno.land/std@v0.12/http/server.ts";
 
-async function main() {
-  const port = Deno.env().PORT;
-  const body = new TextEncoder().encode("Hello World\n");
-  for await (const req of serve(":" + (port ? port : "8080"))) {
+const DEFAULT_PORT = "8080";
+const port = Deno.env().PORT ? Deno.env().PORT : DEFAULT_PORT;
+
+const body = new TextEncoder().encode("Hello World\n");
+const s = serve(":" + DEFAULT_PORT);
+window.onload = async () => {
+  console.log("http://localhost:" + DEFAULT_PORT);
+  for await (const req of s) {
     req.respond({ body });
   }
-}
+};
 
-main();
