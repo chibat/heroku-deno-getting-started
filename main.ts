bafb03bd5b7a5836a8c4b0ee484cd31e010c9e79
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2019-08-02 12:13:13 +0900

    -

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ const DEFAULT_PORT = "8080";
 const port = Deno.env().PORT ? Deno.env().PORT : DEFAULT_PORT;
 
 const body = new TextEncoder().encode("Hello World\n");
-const s = serve(":" + DEFAULT_PORT);
+const s = serve(":" + port);
 window.onload = async () => {
-  console.log("http://localhost:" + DEFAULT_PORT);
+  console.log("http://localhost:" + port);
   for await (const req of s) {
     req.respond({ body });
   }
