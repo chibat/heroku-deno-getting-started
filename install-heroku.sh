a8edf9ffa9b0020a696961f4e6e5c2a7b02057f7
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2020-01-03 08:35:01 +0900

    v0.28.0

---
+++ install-heroku.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl https://cli-assets.heroku.com/install.sh | sh
+
+
--- main.ts
+++ main.ts
@@ -1,20 +1,18 @@
-import { serve } from "https://deno.land/std@v0.27.0/http/server.ts";
-import * as flags from "https://deno.land/std@v0.27.0/flags/mod.ts";
-
+import { serve } from "https://deno.land/std@v0.28.0/http/server.ts";
+import * as flags from "https://deno.land/std@v0.28.0/flags/mod.ts";
 
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(Deno.args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
 if (isNaN(port)) {
-  console.error('Port is not a number.');
+  console.error("Port is not a number.");
   Deno.exit(1);
 }
 
 const body = new TextEncoder().encode("Hello World\n");
-const s = serve({port: port});
+const s = serve({ port: port });
 console.log("http://localhost:" + port);
 for await (const req of s) {
   req.respond({ body });
 }
-
