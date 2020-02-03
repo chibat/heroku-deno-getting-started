70333958856903658730b9d285b00a2c3a6795a1
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2020-02-04 08:32:58 +0900

    v0.32.0

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import { serve } from "https://deno.land/std@v0.31.0/http/server.ts";
-import * as flags from "https://deno.land/std@v0.31.0/flags/mod.ts";
+import { serve } from "https://deno.land/std@v0.32.0/http/server.ts";
+import * as flags from "https://deno.land/std@v0.32.0/flags/mod.ts";
 
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(Deno.args).port;
